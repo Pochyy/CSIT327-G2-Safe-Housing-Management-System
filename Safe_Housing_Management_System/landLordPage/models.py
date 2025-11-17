abd7a4bac1a27d7b0077e1759be2e8c14818c0f1
tashaleonardo <natashakate.leonardo@cit.edu>
2025-11-18 07:15:16 +0800

    fix(models): update Property model for image storage and display

--- Safe_Housing_Management_System/landLordPage/models.py
+++ Safe_Housing_Management_System/landLordPage/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.conf import settings
+import os
 
 class Property(models.Model):
     STATUS_CHOICES = [
@@ -52,6 +53,38 @@ class Property(models.Model):
 
     def __str__(self):
         return f"{self.property_name} ({self.status})"
+    
+    def save(self, *args, **kwargs):
+        """Override save to handle image cleanup"""
+        try:
+            # Get the old instance if it exists
+            if self.pk:
+                old_property = Property.objects.get(pk=self.pk)
+                
+                # Check if image is being changed or removed
+                if old_property.image and old_property.image != self.image:
+                    # Delete the old image file from storage
+                    if os.path.isfile(old_property.image.path):
+                        os.remove(old_property.image.path)
+        except Property.DoesNotExist:
+            pass  # It's a new property, no old image to delete
+        
+        super().save(*args, **kwargs)
+    
+    def delete(self, *args, **kwargs):
+        """Override delete to remove image file"""
+        if self.image:
+            # Delete the image file from storage
+            if os.path.isfile(self.image.path):
+                os.remove(self.image.path)
+        super().delete(*args, **kwargs)
+    
+    @property
+    def image_url(self):
+        if self.image and hasattr(self.image, 'url'):
+            return self.image.url
+        return '/static/images/default-property.jpg'
+
 
 class Notification(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
