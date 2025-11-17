221e42c703184734e5fea465d33a73407e1473ad
tashaleonardo <natashakate.leonardo@cit.edu>
2025-11-18 07:15:32 +0800

    fix(config): update settings and urls for media files

--- Safe_Housing_Management_System/Safe_Housing_Management_System/urls.py
+++ Safe_Housing_Management_System/Safe_Housing_Management_System/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -10,4 +10,5 @@ urlpatterns = [
     path('', include('users.urls')),  
 ]
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
