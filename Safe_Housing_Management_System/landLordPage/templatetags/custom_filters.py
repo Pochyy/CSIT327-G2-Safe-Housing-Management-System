6b9de35416bc4c5cd082fbcc27b5c8c10a6a097d
tashaleonardo <natashakate.leonardo@cit.edu>
2025-11-18 11:11:23 +0800

    feat(templates): add price formatting with commas

--- Safe_Housing_Management_System/landLordPage/templatetags/custom_filters.py
+++ Safe_Housing_Management_System/landLordPage/templatetags/custom_filters.py
@@ -5,3 +5,11 @@ register = template.Library()
 @register.filter
 def split(value, arg):
     return value.split(arg)
+
+@register.filter
+def intcomma(value):
+    try:
+        value = int(float(value))
+        return "{:,}".format(value)
+    except (ValueError, TypeError):
+        return value
