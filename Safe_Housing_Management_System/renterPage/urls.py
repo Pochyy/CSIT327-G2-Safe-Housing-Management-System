0a2a3e8329eeea19f416cf9dda1499466e25cd74
Pochyy <fropachino@gmail.com>
2025-12-05 17:26:10 +0800

    feature(app): comment feature added to property listing

--- Safe_Housing_Management_System/renterPage/urls.py
+++ Safe_Housing_Management_System/renterPage/urls.py
@@ -5,4 +5,6 @@ app_name = 'renter'
 urlpatterns = [
     path('', views.renterPage, name='home'),  
     path('property/<int:property_id>/', views.property_details, name='property_details'), 
+    path('property/<int:property_id>/comment/', views.add_comment, name='add_comment'),
+    path('comment/<int:comment_id>/reply/', views.add_reply, name='add_reply'),
 ]
